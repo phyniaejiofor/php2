8cd96f39e411ec98a153f29061e802041c3a61e1
phyniaejiofor <phyniaejiofor90@gmail.com>
2021-02-23 10:52:49 +0100

    code push on creating and styling  pages with mysql

---
+++ public/staff/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TastyBites Staff</title>
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
+</head>
+<body>
+    <header>
+     <h1>TastyBites Staff Area</h1>
+    </header>
+
+    <navigation>
+      <ul>
+        <li><a href="index.php">Menu</a></li>
+      </ul>
+    </navigation>
+      <div id="content"></div>
+    <footer>
+      &copy; <?php echo date('Y'); ?> Phynia TastyBites
+    </footer>
+</body>
+</html>
